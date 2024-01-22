62dfdeba7f698f70702fddeb55522bcfa9d11265
Ssorreadd <112784857+Ssorreadd@users.noreply.github.com>
2024-01-22 05:33:41 +0300

    Delete TZ directory

--- TZ/14_Semenikhin_TZ1/Assets/DataTypes/LeaderType.cs
+++
@@ -1,7 +0,0 @@
-[System.Serializable]
-public class LeaderType
-{
-    public int Id { get; set; }
-    public string Nickname { get; set; }
-    public int CubeCount { get; set; }
-}
--- TZ/14_Semenikhin_TZ7/Assets/Scripts/IslandController.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class IslandController : MonoBehaviour
-{
-    private void Update()
-    {
-        transform.position -= new Vector3(0, 1f, 0) * Time.deltaTime;
-    }
-}
